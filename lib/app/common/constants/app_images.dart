6d8c98b6162ab1e875da6d48fa10f92d745ec843
AbstractProgrammer@example.com <AbstractProgrammer@example.com>
2025-04-06 13:00:43 +0530

    added constants

--- lib/app/common/constants/app_images.dart
+++ lib/app/common/constants/app_images.dart
@@ -1,4 +1,53 @@
-abstract class AppImages {
-  static const String imagePath='assets/images';
-  static const String appSplachLogoImagePNG='$imagePath/img_splash_logo.png';
+// abstract class AppImages {
+//   static const String imagePath='assets/images';
+//   static const String appSplachLogoImagePNG='$imagePath/img_splash_logo.png';
+// }
+
+
+class AppImages{
+    static const String imagePath='assets/images';
+  //splash logo
+  static const String splashlogo="assets/images/logos/demo1.png";
+
+  // social logos
+  static const String facebook="assets/images/logos/facebook.png";
+  static const String google="assets/images/logos/googlelogo.png";
+//onboarding
+  static const String onBoardingimage1="assets/images/onboard1.png";
+  static const String onBoardingimage2="assets/images/onbaoard2.png";
+  static const String onBoardingimage3="assets/images/onboard3.png";
+  //Email verification
+  static const String Emailverification1="assets/images/mailsend.png";
+  static const String Emailverification2="assets/images/logos/email verified.png";
+  //icons
+  static const String popularIcon1="assets/images/logos/icons/iconshoe.png";
+
+  //  banners
+  static const String banner1="assets/images/banners/banner1.jpg";
+  static const String banner2="assets/images/banners/banner2.jpg";
+  static const String banner3="assets/images/banners/banner3.jpg";
+  //product
+  static const String productimage1="assets/images/product/productimage11.png";
+  static const String productimage2="assets/images/product/productimage2.jpg";
+  static const String productimage3="assets/images/product/productimage3.jpg";
+  static const String productimage4="assets/images/product/productimage4.jpg";
+  static const String productimage5="assets/images/product/productimage5.jpg";
+  static const String productimage6="assets/images/product/productimage6.jpg";
+  //productlogos
+  static const String Brandlogo1="assets/images/logos/cloth logo.png";
+  static const String Branlogo2="assets/images/logos/shoe logo.png";
+  //proile
+  static const String profile="assets/images/logos/icons/profile.png";
+  //payment
+  static const String paypal="assets/images/logos/payment/paypal-removebg-preview.png";
+  static const  String phonepay="assets/images/logos/payment/phonepay-removebg-preview.png";
+  static const String paytm="assets/images/logos/payment/images-removebg-preview.png";
+  static const String googlepay="assets/images/logos/payment/googlpay-removebg-preview.png";
+  //success
+  static const String success="assets/images/logos/icons/success.png";
+  // lottie
+  static const String  lot="assets/images/logos/Animation - 1727769480794.json";
+
+
+
 }
--- lib/app/modules/home/widgets/category_grid_list.dart
+++ lib/app/modules/home/widgets/category_grid_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:innsouls_flutter/app/modules/home/widgets/category_tile.dart';
 
 class CategoryGridList extends StatelessWidget {
